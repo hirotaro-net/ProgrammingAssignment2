978403500d527d641d09aee2ccff20d2ce780570
Hirotaro Matsumoto <matsumoto@hirotaro.net>
2022-09-18 16:12:55 +0800

    Initial Commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    im <- NULL
+    set <- function(y) {
+        x <<- y
+        im <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) im <<- inverse
+    getInverse <- function() im
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +20,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    im <- x$getInverse()
+    if(!is.null(im)) {
+        print('getting cached inverse matrix')
+        return(im)
+    }
+    data <- x$get()
+    im <- solve(data, ...)
+    x$setInverse(im)
+    im
 }
+
+B <- matrix(c(1,2,3,4),2,2)
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1)
+cacheSolve(B1)
